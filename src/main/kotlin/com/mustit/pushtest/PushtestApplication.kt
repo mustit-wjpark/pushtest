73bde69603a6acde96f3001f4bb874cb03e5fb1e
mustit-wjpark <118790777+mustit-wjpark@users.noreply.github.com>
2022-12-06 17:37:38 +0900

    init: 프로젝트 최초 커밋

---
+++ src/main/kotlin/com/mustit/pushtest/PushtestApplication.kt
@@ -0,0 +1,11 @@
+package com.mustit.pushtest
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PushtestApplication
+
+fun main(args: Array<String>) {
+	runApplication<PushtestApplication>(*args)
+}
---
+++ src/test/kotlin/com/mustit/pushtest/PushtestApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.mustit.pushtest
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PushtestApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
